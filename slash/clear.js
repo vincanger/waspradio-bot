7438e012b91cd308e69579de87fa2860ca007a12
vincanger <70215737+vincanger@users.noreply.github.com>
2022-12-13 17:40:54 +0100

    clear

--- slash/clear.js
+++ slash/clear.js
@@ -2,14 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('quit')
-    .setDescription('Stop the music and leave the voice channel'),
+    .setName('clear')
+    .setDescription('Clear the queue'),
   run: async ({ client, interaction }) => {
     const queue = client.player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
       return await interaction.editReply({ content: 'There is no music playing in this server!', ephemeral: true });
-    queue.destroy();
-    await interaction.editReply({ content: 'Music stopped and left the voice channel!' });
+    queue.clear();
+    await interaction.editReply({ content: 'Queue cleared!' });
   }
 };
 
